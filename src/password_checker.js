ebd891402027b0e7e074a129640b5028ff1b992a
Siyabonga Samson Lukhele <110550090+markuptitan@users.noreply.github.com>
2024-06-13 09:35:32 +0000

    Add array with conditions to check

--- src/password_checker.js
+++ src/password_checker.js
@@ -3,6 +3,14 @@ function passwordStrength(password) {
   if (password.length <= 8) {
     return "invalid";
   }
+  let conditionsMet = 0;
+  const patterns = [
+    strengthCheckerPatterns.lowerCaseCharacter,
+    strengthCheckerPatterns.upperCaseCharacter,
+    strengthCheckerPatterns.digitCharacter,
+    strengthCheckerPatterns.specialCaseCharacter,
+    strengthCheckerPatterns.whitespaceCharacter,
+  ];
 }
 
 console.log(passwordStrength(""));
