5da51483c8b1e33585af8665d071f847d4d3da8b
Siyabonga Samson Lukhele <110550090+markuptitan@users.noreply.github.com>
2024-06-13 09:33:31 +0000

    Remove all code to follow a more DRY approach

--- src/password_checker.js
+++ src/password_checker.js
@@ -1,38 +1,6 @@
 const { strengthCheckerPatterns } = require("./helper_objects");
 function passwordStrength(password) {
-    if (password.length <= 8) {
-      return "invalid";
-    }
-    let conditionsMet = 0;
-    if (password.length !== 0) {
-      conditionsMet++;
-    }
-    if (password.length > 8) {
-      conditionsMet++;
-    }
-    if (strengthCheckerPatterns.lowerCaseCharacter.test(password)) {
-      conditionsMet++;
-    }
-    if (strengthCheckerPatterns.upperCaseCharacter.test(password)) {
-      conditionsMet++;
-    }
-    if (strengthCheckerPatterns.digitCharacter.test(password)) {
-      conditionsMet++;
-    }
-    if (strengthCheckerPatterns.specialCaseCharacter.test(password)) {
-      conditionsMet++;
-    }
-    if (strengthCheckerPatterns.whitespaceCharacter.test(password)) {
-      conditionsMet++;
-    }
-
-  if (conditionsMet >= 6) {
-    return "strong";
-  } else if (conditionsMet >= 4) {
-    return "medium";
-  } else if (conditionsMet == 3) {
-    return "weak";
-  } else {
+  if (password.length <= 8) {
     return "invalid";
   }
 }
