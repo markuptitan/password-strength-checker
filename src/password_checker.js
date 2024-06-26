7eb502755c53575c544bab78838cff9b243bc438
Siyabonga Samson Lukhele <110550090+markuptitan@users.noreply.github.com>
2024-06-26 15:17:46 +0200

    Create password_checker.js

---
+++ src/password_checker.js
@@ -0,0 +1,31 @@
+const { strengthCheckerPatterns } = require("./helper_objects");
+function passwordStrength(password) {
+  if (password.length <= 8) {
+    return "invalid";
+  }
+  let conditionsMet = 2;
+  const patterns = [
+    strengthCheckerPatterns.lowerCaseCharacter,
+    strengthCheckerPatterns.upperCaseCharacter,
+    strengthCheckerPatterns.digitCharacter,
+    strengthCheckerPatterns.specialCaseCharacter,
+    strengthCheckerPatterns.whitespaceCharacter,
+  ];
+  for (const pattern of patterns) {
+    if (pattern.test(password)) {
+      conditionsMet++;
+    }
+  }
+  if (conditionsMet >= 6) {
+    return "strong";
+  } else if (conditionsMet >= 4) {
+    return "medium";
+  } else if (conditionsMet == 3) {
+    return "weak";
+  } else {
+    return "invalid";
+  }
+}
+
+module.exports = { passwordStrength };
+  
