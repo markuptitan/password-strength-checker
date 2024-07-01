5c3e0745cf0ee1c77baed89993f2f1f4a605c4c0
Siyabonga Samson Lukhele <110550090+markuptitan@users.noreply.github.com>
2024-07-01 14:09:11 +0200

    Update password_checker_spec.js

--- spec/password_checker_spec.js
+++ spec/password_checker_spec.js
@@ -2,20 +2,22 @@ const { passwordStrength } = require("./../src/password_checker");
 
 describe("passwordStrength", function () {
   it("Returns invalid for empty passwords and passwords with less than 9 characers", function () {
-    expect(passwordStrength("")).toEqual("invalid");
-    expect(passwordStrength("Numberz0")).toEqual("invalid");
+    expect(passwordStrength("")).toBe("invalid");
+    expect(passwordStrength("Numberz0")).toBe("invalid");
   });
   it("Returns weak for passwords meeting exactly 3 conditions", function () {
-    expect(passwordStrength("samsonlukhele")).toEqual("weak");
-    expect(passwordStrength("siyabonga")).toEqual("weak");
+    expect(passwordStrength("samsonlukhele")).toBe("weak");
+    expect(passwordStrength("siyabonga")).toBe("weak");
   });
   it("Returns medium for passwords meeting 4 - 5 conditions", function () {
-    expect(passwordStrength("SamsonManOfPeace32")).toEqual("medium");
-    expect(passwordStrength("SiyabongaLukhele32")).toEqual("medium");
+    expect(passwordStrength("SamsonManOfPeace32")).toBe("medium");
+    expect(passwordStrength("SiyabongaLukhele32")).toBe("medium");
   });
   it("Returns strong for passwords meeting 6 or more conditions", function () {
-    expect(passwordStrength("SamsonManOfPeace32.")).toEqual("strong");
-    expect(passwordStrength("Siyabonga Lukhele32!")).toEqual("strong");
+    expect(passwordStrength("SamsonManOfPeace32.")).toBe("strong");
+    expect(passwordStrength("Siyabonga Lukhele32!")).toBe("strong");
   });
-
+  it("should return 'weak' for an input of space characters that satisfy the required password length", function () {
+    expect(passwordStrength("           ")).toBe(weak);
+  });
 });
