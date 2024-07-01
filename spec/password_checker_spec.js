a68988e885023b2ea496cc99ee426a4ce29b7282
Siyabonga Samson Lukhele <110550090+markuptitan@users.noreply.github.com>
2024-07-01 14:09:52 +0200

    Update password_checker_spec.js

--- spec/password_checker_spec.js
+++ spec/password_checker_spec.js
@@ -1,19 +1,19 @@
 const { passwordStrength } = require("./../src/password_checker");
 
-describe("passwordStrength", function () {
-  it("Returns invalid for empty passwords and passwords with less than 9 characers", function () {
+describe("passwordStrength():", function () {
+  it("returns invalid for empty passwords and passwords with less than 9 characers", function () {
     expect(passwordStrength("")).toBe("invalid");
     expect(passwordStrength("Numberz0")).toBe("invalid");
   });
-  it("Returns weak for passwords meeting exactly 3 conditions", function () {
+  it("returns weak for passwords meeting exactly 3 conditions", function () {
     expect(passwordStrength("samsonlukhele")).toBe("weak");
     expect(passwordStrength("siyabonga")).toBe("weak");
   });
-  it("Returns medium for passwords meeting 4 - 5 conditions", function () {
+  it("returns medium for passwords meeting 4 - 5 conditions", function () {
     expect(passwordStrength("SamsonManOfPeace32")).toBe("medium");
     expect(passwordStrength("SiyabongaLukhele32")).toBe("medium");
   });
-  it("Returns strong for passwords meeting 6 or more conditions", function () {
+  it("returns strong for passwords meeting 6 or more conditions", function () {
     expect(passwordStrength("SamsonManOfPeace32.")).toBe("strong");
     expect(passwordStrength("Siyabonga Lukhele32!")).toBe("strong");
   });
