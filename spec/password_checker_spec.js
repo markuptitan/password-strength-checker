4d8840c6aca75dd6479ef9a00869d2e6dff14c0b
markuptitan <samsonlukhele76@gmail.com>
2024-07-01 16:57:15 +0200

    Fix test descriptions

--- spec/password_checker_spec.js
+++ spec/password_checker_spec.js
@@ -4,19 +4,18 @@ const {
 } = require("./../src/password_checker");
 
 describe("passwordStrength():", function () {
-  it("returns invalid for empty passwords and passwords with less than 9 characers", function () {
-    expect(passwordStrength("Numberz0")).toBe(strengthMessages.invalid);
+  it("should return 'invalid' for empty passwords and passwords with less than 9 characers", function () {
+    expect(passwordStrength("Numberz0")).toBe(strengthMessages.isInvalid);
   });
-  it("returns weak for passwords meeting exactly 3 conditions", function () {
-    expect(passwordStrength("samsonlukhele")).toBe("weak");
+  it("should return 'weak' for passwords meeting exactly 3 conditions", function () {
     expect(passwordStrength("siyabonga")).toBe(strengthMessages.isWeak);
   });
-  it("returns medium for passwords meeting 4 - 5 conditions", function () {
+  it("should return 'medium' for passwords meeting 4 - 5 conditions", function () {
     expect(passwordStrength("SamsonManOfPeace32")).toBe(
       strengthMessages.isMedium
     );
   });
-  it("returns strong for passwords meeting 6 or more conditions", function () {
+  it("should return 'strong' for passwords meeting 6 or more conditions", function () {
     expect(passwordStrength("Siyabonga Lukhele32!")).toBe(
       strengthMessages.isStrong
     );
