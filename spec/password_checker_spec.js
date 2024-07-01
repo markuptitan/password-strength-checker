ba7d8ed4b41526a5f4a63e7dfac36ec8d1cc0a98
markuptitan <samsonlukhele76@gmail.com>
2024-07-01 16:54:14 +0200

    Strictly have 1 test per it block

--- spec/password_checker_spec.js
+++ spec/password_checker_spec.js
@@ -1,21 +1,25 @@
-const { passwordStrength } = require("./../src/password_checker");
+const {
+  passwordStrength,
+  strengthMessages,
+} = require("./../src/password_checker");
 
 describe("passwordStrength():", function () {
   it("returns invalid for empty passwords and passwords with less than 9 characers", function () {
-    expect(passwordStrength("")).toBe("invalid");
-    expect(passwordStrength("Numberz0")).toBe("invalid");
+    expect(passwordStrength("Numberz0")).toBe(strengthMessages.invalid);
   });
   it("returns weak for passwords meeting exactly 3 conditions", function () {
     expect(passwordStrength("samsonlukhele")).toBe("weak");
-    expect(passwordStrength("siyabonga")).toBe("weak");
+    expect(passwordStrength("siyabonga")).toBe(strengthMessages.isWeak);
   });
   it("returns medium for passwords meeting 4 - 5 conditions", function () {
-    expect(passwordStrength("SamsonManOfPeace32")).toBe("medium");
-    expect(passwordStrength("SiyabongaLukhele32")).toBe("medium");
+    expect(passwordStrength("SamsonManOfPeace32")).toBe(
+      strengthMessages.isMedium
+    );
   });
   it("returns strong for passwords meeting 6 or more conditions", function () {
-    expect(passwordStrength("SamsonManOfPeace32.")).toBe("strong");
-    expect(passwordStrength("Siyabonga Lukhele32!")).toBe("strong");
+    expect(passwordStrength("Siyabonga Lukhele32!")).toBe(
+      strengthMessages.isStrong
+    );
   });
   it("should return 'weak' for an input of space characters that satisfy the required password length", function () {
     expect(passwordStrength("           ")).toBe("weak");
